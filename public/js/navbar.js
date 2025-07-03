c1b736d1a6ca9123112cce755b237990e26bf9dd
frncsge <francisge527@gmail.com>
2025-07-03 12:20:00 +0800

    add navigation bar responsiveness and side bar functionality

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -5,6 +5,31 @@ const searchButton = document.getElementById("search-button");
 const searchDropDown = document.getElementById("search-dropdown");
 const dropDownLiCon = document.getElementById("search-dropdown-li-container");
 
+const sideNav = document.getElementById("side-nav-menu");
+const menuButton = document.getElementsByClassName("menu-container")[0];
+
+if (window.innerWidth > 600) {
+  sideNav.classList.remove("show");
+  menuButton.children[0].classList.remove("change");
+  menuButton.children[1].classList.remove("change");
+  menuButton.children[2].classList.remove("change");
+}
+
+//function for opening the side bar menu for mobile phones
+function openMenu(menu) {
+  menu.classList.toggle("change");
+  sideNav.classList.toggle("show");
+}
+
+//function to check if screen size is more than 600px to remove the side bar and menu icon
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 600) {
+    sideNav.classList.remove("show");
+    //this resets the animation of the menu instead of staying looking like X lol
+    menuButton.classList.remove("change");
+  }
+});
+
 //function that send a request to the server using AJAX when search is clicked and the input isn't empty
 async function runSearch() {
   dropDownLiCon.innerHTML = "";
